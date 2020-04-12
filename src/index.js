61743d5ecf617cc67f7f0c033453482ed6369e87
Rithesh-Rithu <ritheshdavid@gmail.com>
2020-04-12 12:55:01 +0530

    "Header and Footer"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
